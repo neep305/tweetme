afeb30f47139d006005260eea9466fa849c988a2
neep305 <neep305@gmail.com>
2020-04-29 18:07:45 +0900

    add list view

--- tweets/views.py
+++ tweets/views.py
@@ -7,20 +7,33 @@ from tweets.models import Tweet
 
 
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id":id, "content": x.content} for x in qs]
+    data = {
+        "response": tweets_list
+    }
+    return JsonResponse(data)
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    try:
-        obj = Tweet.objects.get(id=tweet_id)
-    except:
-        return HttpResponseNotFound('<h1>Page not found</h1>')
-
-    # return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
     data = {
         "id": tweet_id,
-        "content": obj.content,
+        # "content": obj.content,
         # "image_path": obj.image.url
     }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content
+    except:
+        # return HttpResponseNotFound('<h1>Page not found</h1>')
+        data['message'] = 'Not found'
+        status = 404
+    # return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
 
-    return JsonResponse(data)
+    return JsonResponse(data, status=status)
